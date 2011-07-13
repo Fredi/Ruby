5136cfb7ec03b0739e9b6a2de9f5fe0833315d14
Fredi Machado <fredisoft@gmail.com>
2011-07-13 10:28:25 -0300

    times, upto and downto methods

---
+++ 13.rb
@@ -0,0 +1,29 @@
+# times, upto and downto methods
+
+# times
+3.times { |i| puts i }
+
+puts "\n"
+
+# upto
+5.upto(10) do
+  puts "hello"
+end
+
+puts "\n"
+
+# same
+5.upto(10) { |i| puts i }
+
+puts "\n"
+
+# downto
+15.downto(10) do
+  puts "hello"
+end
+
+puts "\n"
+
+# same
+15.downto(10) { |i| puts i }
+
