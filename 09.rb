af2eb80e5007054da848af2eb037dc88b84cd3ab
Fredi Machado <fredisoft@gmail.com>
2011-07-13 09:27:58 -0300

    Converting variable values

---
+++ 09.rb
@@ -0,0 +1,12 @@
+# converting variables values
+
+a = 10
+puts a
+puts a.to_f # to float
+
+b = 200
+puts b.class
+puts b.to_s.class # string
+puts b.to_s(2) # conver to binary specifying a number base of 2
+puts b.to_s(16) # hexadecimal
+puts b.to_s(8) # octal
